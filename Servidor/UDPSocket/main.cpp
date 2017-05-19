e41ebf12774d289dd36c920e975e5e3261516fa2
robertophi <robertophi@gmail.com>
2017-05-19 16:28:28 -0300

    UDP Socket no QT

---
+++ Servidor/UDPSocket/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include "myudp.h"
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyUDP server;
+    MyUDP client;
+    client.SayHello();
+    server.SayHello();
+    return a.exec();
+}
